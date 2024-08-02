c0321fd1d0040e8e3fdc67cbc32d2e2d5c0a4de8
Luis Velasquez <luis.velasquezg@udea.edu.co>
2024-08-02 11:01:15 -0500

    refactor(Front): :recycle: Conversión inicial de archivos JS a TS en AngularJS
    Se convierten los archivos JavaScript a TypeScript y se emplea estructura de Módulo

---
+++ front/angularjs/modules/archivos/archivos.ts
@@ -0,0 +1,11 @@
+// Module of Archivos
+import "./archivos.client.module";
+
+// Configuring the Archivos module
+import "./config/archivos.client.routes";
+
+// Controller of Archivos
+import "./controllers/archivos.client.controller";
+
+// Service of Archivos
+import "./services/archivos.client.service";
